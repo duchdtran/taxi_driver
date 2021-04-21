ba0ecc2f840097bbaf2426bd2641350919f0c4b7
duchdtran <duc.td173028@sis.hust.edu.vn>
2021-04-21 23:37:48 +0700

    Refs #9: Develop sign up thanks screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'views/screens/sign_up/documents/index.dart';
 import 'views/screens/sign_up/index.dart';
 import 'views/screens/sign_up/otp/index.dart';
 import 'views/screens/sign_up/personal/index.dart';
+import 'views/screens/sign_up/thanks/index.dart';
 import 'views/screens/welcome/index.dart';
 
 void main() {
@@ -26,6 +27,7 @@ class MyApp extends StatelessWidget {
         AppRouters.signUpPersonalScreen: (context) => SignUpPersonalScreen(),
         AppRouters.signUpBankCardScreen: (context) => SignUpBankCardScreen(),
         AppRouters.signUpDocumentsScreen: (context) => SignUpDocumentsScreen(),
+        AppRouters.signUpThanksScreen: (context) => SignUpThanksScreen(),
       },
     );
   }
